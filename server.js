49da7fc3c8417e93037b749bc2062e446ac824ae
DaniPhilo <daniel.carrasco.casado@gmail.com>
2022-05-11 17:35:00 +0200

    Modified static route for react files

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, "/client")));
+app.use(express.static(path.join(__dirname, "/client/build")));
 
 // API Routes:
 const landingRoutes = require('./routes/landing_routes');
