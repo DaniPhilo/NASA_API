72f4b489ab34b7a3a00d30a54d91ce97d2d0709f
DaniPhilo <daniel.carrasco.casado@gmail.com>
2022-05-11 17:01:36 +0200

    Change order of middlewares: deployed in Heroku, but route / not found

--- server.js
+++ server.js
@@ -17,13 +17,13 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.resolve(__dirname, ".client/build")));
+
 
 // Routes:
 const landingRoutes = require('./routes/landing_routes');
 
 app.use('/api/astronomy/landings', landingRoutes);
-
+app.use(express.static(path.resolve(__dirname, ".client/build")));
 // Error handlers:
 app.use((err, req, res, next) => {
     if (err.type === 'custom_error') {
